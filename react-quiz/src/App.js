195233649d89608c8c426065f55cc07f15249f5d
ravi rawat <ravirawat0@outlook.com>
2023-07-24 01:16:55 +0530

    m : Added basic feature: * Added showing Question and selecting the write answer

---
+++ react-quiz/src/App.js
@@ -0,0 +1,75 @@
+import { useEffect, useReducer } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import StartScreen from "./components/StartScreen";
+import Question from "./components/Question";
+
+const initialState = {
+  questions: [],
+  // active, ready, error, finished
+  status: "loading",
+  index: 0,
+  answer: null,
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    case "start":
+      return {
+        ...state,
+        status: "start",
+      };
+    case "newAnswer":
+      return {
+        ...state,
+        answer: action.payload,
+      };
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
+function App() {
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+  const numQuestions = questions.length;
+  console.log(questions[0]);
+
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "start" && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
+      </Main>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-quiz/src/components/Error.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Error() {
+  return (
+    <p className="error">
+      <span>⛔</span>
+      There was an error fetching questions.
+    </p>
+  );
+}
+
+export default Error;
